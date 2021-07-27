b4d9ba525ca97b78343aa3cdd6be0605e3d19283
browndw <davidwestbrown@gmail.com>
2021-07-27 18:10:30 -0400

    Update google_ngrams.R

--- R/google_ngrams.R
+++ R/google_ngrams.R
@@ -30,7 +30,7 @@ google_ngram <- function(word_forms, variety=c("eng", "gb", "us", "fiction"), by
 
   grep_words <- paste0("^", word_forms, "$", collapse = "|")
   all_grams <- suppressWarnings(readr::read_tsv_chunked(repo, col_names = FALSE, col_types = list(X1 = readr::col_character(), X2 = readr::col_double(), X3 = readr::col_double(), X4 = readr::col_double()), quote = "", callback = readr::DataFrameCallback$new(function(x, pos) subset(x, grepl(grep_words, x$X1, ignore.case=TRUE))), progress = T))
-  colnames(all_grams) <- c("token", "year", "token_count", "pages")
+  colnames(all_grams) <- c("token", "Year", "AF", "pages")
 
   if(variety == "eng") repo_total <-("http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-totalcounts-20120701.txt")
   if(variety != "eng") repo_total <-paste0("http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-", variety, "-all-totalcounts-20120701.txt")
@@ -39,20 +39,20 @@ google_ngram <- function(word_forms, variety=c("eng", "gb", "us", "fiction"), by
   total_counts <- as.data.frame(t(total_counts))
   total_counts <- data.frame(V1 = (total_counts[!is.na(total_counts),]))
   total_counts <- data.frame(do.call("rbind", strsplit(as.character(total_counts$V1), "," ,fixed = TRUE)))
-  colnames(total_counts) <- c("year", "total_count", "page_count", "volume_count")
-  total_counts$total_count <- as.numeric(as.character(total_counts$total_count))
-  total_counts$decade <- gsub("\\d$", "0", total_counts$year)
-  if (by == "year") total_counts <- aggregate(total_count ~ year, total_counts, sum)
-  if (by == "decade") total_counts <- aggregate(total_count ~ decade, total_counts, sum)
+  colnames(total_counts) <- c("Year", "Total", "page_count", "volume_count")
+  total_counts$Total <- as.numeric(as.character(total_counts$Total))
+  total_counts$Decade <- gsub("\\d$", "0", total_counts$Year)
+  if (by == "year") total_counts <- aggregate(Total ~ Year, total_counts, sum)
+  if (by == "decade") total_counts <- aggregate(Total ~ Decade, total_counts, sum)
 
   all_tokens <- subset(all_grams, grepl(grep_words, all_grams$token, ignore.case=TRUE))
   all_tokens$token <- tolower(all_tokens$token)
-  sum_tokens <- aggregate(token_count ~ year, all_tokens, sum)
-  sum_tokens$decade <- gsub("\\d$", "0", sum_tokens$year)
-  if (by == "decade") sum_tokens <- aggregate(token_count ~ decade, sum_tokens, sum)
-  if (by == "year") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "year")
-  if (by == "decade") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "decade")
-  counts_norm <- mapply(function(x,y) (x/y)*1000000, sum_tokens$token_count, sum_tokens$total_count)
-  sum_tokens$counts_permil <- counts_norm
+  sum_tokens <- aggregate(AF ~ Year, all_tokens, sum)
+  sum_tokens$Decade <- gsub("\\d$", "0", sum_tokens$Year)
+  if (by == "decade") sum_tokens <- aggregate(AF ~ Decade, sum_tokens, sum)
+  if (by == "year") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Year")
+  if (by == "decade") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Decade")
+  counts_norm <- mapply(function(x,y) (x/y)*1000000, sum_tokens$AF, sum_tokens$Total)
+  sum_tokens$Per_10.6 <- counts_norm
   return(sum_tokens)
 }
