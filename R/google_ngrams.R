758cadf5edbc30e6944150b0b3bd659f5d509b1e
browndw <davidwestbrown@gmail.com>
2021-07-28 16:49:13 -0400

    Update google_ngrams.R

--- R/google_ngrams.R
+++ R/google_ngrams.R
@@ -18,13 +18,21 @@
 #' @return A data.frame of counts from Google Books
 #' @export
 google_ngram <- function(word_forms, variety=c("eng", "gb", "us", "fiction"), by=c("year", "decade")){
-  n <- lapply(word_forms, function(x) stringr::str_count(x, "\\w+"))
+  word_forms <- stringr::str_replace_all(word_forms, "([a-zA-z0-9])-([a-zA-z0-9])", "\\1 - \\2")
+  word_forms <- stringr::str_squish(word_forms)
+  n <- lapply(word_forms, function(x) stringr::str_count(x, "\\S+"))
   n <- unique(n)
-  if (length(n) > 1)  stop ("Check spelling. Word forms should be lemmas of the same word (e.g. 'teenager' and 'teenagers' or 'walk' , 'walks' and 'walked'")
-  gram <- lapply(word_forms, function(x) substring(x, 1, n))
+  if(length(n) > 1)  stop ("Check spelling. Word forms should be lemmas of the same word (e.g. 'teenager' and 'teenagers' or 'walk' , 'walks' and 'walked'")
+  if(n > 5)  stop ("Ngrams can be a maximum of 5 tokens. Hyphenated words are also split and include the hyphen, so 'x-ray' would count as 3 tokens.")
+  gram <- ifelse(n > 1, lapply(word_forms, function(x) substring(x, 1, 2)), lapply(word_forms, function(x) substring(x, 1, 1)))
   gram <- tolower(unique(gram))
-  if (length(gram) > 1)  stop ("Check spelling. Word forms should be lemmas of the same word (e.g. 'teenager' and 'teenagers' or 'walk' , 'walks' and 'walked'")
-
+  if(length(gram) > 1)  stop ("Check spelling. Word forms should be lemmas of the same word (e.g. 'teenager' and 'teenagers' or 'walk' , 'walks' and 'walked'")
+  if(stringr::str_detect(gram, "^[a-z][^a-z]")) gram <- stringr::str_replace(gram, "[^a-z]", "_")
+  if(stringr::str_detect(gram, "^[0-9]")) gram <- substring(gram, 1, 1)
+  if(stringr::str_detect(gram, "^[[:punct:]]")) gram <- "punctuation"
+  if(any(stringr::str_detect(substring(gram, 1, 1), c("ß", "æ", "ð", "ø", "ł", "œ", "ı", "ƒ", "þ", "ȥ", "ə", "ħ", "ŋ", "ª", "º", "ɣ", "đ", "ĳ", "ɔ", "ȝ", "ⅰ", "ʊ", "ʌ", "ʔ", "ɛ", "ȡ", "ɋ", "ⅱ", "ʃ", "ɇ", "ɑ", "ⅲ")))) gram <- "other"
+  gram <- stringi::stri_trans_general(gram, "Latin-ASCII")
+  
   if(variety == "eng") repo <- paste0("http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-", n, "gram-20120701-", gram, ".gz")
   if(variety != "eng") repo <- paste0("http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-", variety, "-all-", n, "gram-20120701-", gram, ".gz")
 
@@ -42,16 +50,15 @@ google_ngram <- function(word_forms, variety=c("eng", "gb", "us", "fiction"), by
   colnames(total_counts) <- c("Year", "Total", "page_count", "volume_count")
   total_counts$Total <- as.numeric(as.character(total_counts$Total))
   total_counts$Decade <- gsub("\\d$", "0", total_counts$Year)
-  if (by == "year") total_counts <- aggregate(Total ~ Year, total_counts, sum)
-  if (by == "decade") total_counts <- aggregate(Total ~ Decade, total_counts, sum)
+  if(by == "year") total_counts <- aggregate(Total ~ Year, total_counts, sum)
+  if(by == "decade") total_counts <- aggregate(Total ~ Decade, total_counts, sum)
 
-  all_tokens <- subset(all_grams, grepl(grep_words, all_grams$token, ignore.case=TRUE))
-  all_tokens$token <- tolower(all_tokens$token)
-  sum_tokens <- aggregate(AF ~ Year, all_tokens, sum)
-  if (by == "decade") sum_tokens$Decade <- gsub("\\d$", "0", sum_tokens$Year)
-  if (by == "decade") sum_tokens <- aggregate(AF ~ Decade, sum_tokens, sum)
-  if (by == "year") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Year")
-  if (by == "decade") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Decade")
+  all_grams$token <- tolower(all_grams$token)
+  sum_tokens <- aggregate(AF ~ Year, all_grams, sum)
+  if(by == "decade") sum_tokens$Decade <- gsub("\\d$", "0", sum_tokens$Year)
+  if(by == "decade") sum_tokens <- aggregate(AF ~ Decade, sum_tokens, sum)
+  if(by == "year") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Year")
+  if(by == "decade") sum_tokens <- merge(sum_tokens, y = total_counts[,c(1:2)], by = "Decade")
   counts_norm <- mapply(function(x,y) (x/y)*1000000, sum_tokens$AF, sum_tokens$Total)
   sum_tokens$Per_10.6 <- counts_norm
   return(sum_tokens)
